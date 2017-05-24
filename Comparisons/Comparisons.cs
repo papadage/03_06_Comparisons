edf815c101c16946c1a80a2d36eadee01a8fc3d7
papadage <gpapadakis@aegis-net.com>
2017-05-24 19:39:07 -0400

    Add project files.

---
+++ Comparisons/Comparisons.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Comparisons
+{
+   static void Main()
+   {
+      Console.WriteLine("Please provide the first number:");
+      int number1 = int.Parse(Console.ReadLine());
+
+      Console.WriteLine("Please provide the second number:");
+      int number2 = int.Parse(Console.ReadLine());
+
+      int sum = number1 + number2;
+
+      Console.WriteLine($"The sum of the two numbers is {sum}.");
+
+      if (number1 == number2)
+      {
+         Console.WriteLine($"{number1} = {number2}");
+      }
+
+      if (number1 != number2)
+      {
+         Console.WriteLine($"{number1} /= {number2}");
+      }
+
+      if (number1 > number2)
+      {
+         Console.WriteLine($"{number1} > {number2}");
+      }
+
+      if (number1 < number2)
+      {
+         Console.WriteLine($"{number1} < {number2}");
+      }
+
+      if (number1 >= number2)
+      {
+         Console.WriteLine($"{number1} => {number2}");
+      }
+
+      if (number1 <= number2)
+      {
+         Console.WriteLine($"{number1} <= {number2}");
+      }
+   }
+}
